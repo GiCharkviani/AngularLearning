7f333c96fc0a86702039b710c8d368cb7b48e487
GiCharkviani <gi.charkviani@gmail.com>
2021-10-20 17:36:54 +0400

    updated today

---
+++ 5. NgRx/ngrx-course/src/app/reducers/index.ts
@@ -0,0 +1,29 @@
+import { routerReducer } from '@ngrx/router-store';
+import {
+  Action,
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+
+export interface State {
+
+}
+
+export const reducers: ActionReducerMap<State> = {
+  router: routerReducer
+};
+
+export function logger(reducer:ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log("state before: ", state);
+    console.log("action");
+    return reducer(state, action)
+  }
+}
+
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger] : [];
